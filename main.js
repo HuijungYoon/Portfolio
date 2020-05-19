4f5b9eb1cf91a0209e82ee8524caec3f575e736c
HuijungYoon <markup3604@gmail.com>
2020-05-19 10:25:39 +0900

    JS:Testimonials

--- main.js
+++ main.js
@@ -11,3 +11,17 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 });
+
+//Handle scrolling when tappng on the navbar menu
+
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (e) => {
+  console.log(e.target.dataset.link);
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+});
